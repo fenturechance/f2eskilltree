d8906bc8385ec078ac88469c14461384a69caf4f
Jerry <fenturechance@gmail.com>
2018-07-31 17:55:27 +0800

    firs

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+      <div class="titleWrapper"></div>
+      <div class="treeWrapper"></div>
+      <div class="descriptionWrapper"></div>
+      <div class="footerWrapper"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+</style>

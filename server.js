e0ed5b2f4049d26114e6ac37639804623485dd30
Jerry <fenturechance@gmail.com>
2018-08-06 09:20:54 +0800

    heroku set

---
+++ server.js
@@ -0,0 +1,8 @@
+let express = require('express');
+let path = require('path');
+let serverStatic = require('server-static');
+app = express();
+app.use(serverStatic(`${__dirname}/dist`));
+let port = process.env.PORT || 5000;
+app.listen(port);
+console.log(`server started ${port}`);
